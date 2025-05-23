e076e6b6040f20569138e4668d7dcd4ab1642ec6
Denismaka <makadenis370@gmail.com>
2025-05-23 15:04:09 +0100

    Debut d'apprentissage des cookies

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <title>Apprentissage des cookies avec php</title>
+</head>
+
+<body class="bg-dark">
+
+    <!-- ############## -->
+    <!-- cookies -->
+    <!-- ############## -->
+
+
+</body>
+
+</html>
+
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
