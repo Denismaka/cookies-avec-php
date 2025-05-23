b594e9bbd3bee7f70b4a6c26c414ea1b989649cd
Denismaka <makadenis370@gmail.com>
2025-05-23 17:28:11 +0100

    Mise en place de la page complexe pour apprendre comment utiliser des cookies de manière un peut complexe

---
+++ complexe.php
@@ -0,0 +1,28 @@
+<?php
+include('./header.php')
+?>
+<!-- ################### -->
+<!-- Infos plus complexe -->
+<!-- ################### -->
+<?php if ($nom): ?>
+    <h1>Bonjour <?= htmlentities($nom) ?></h1>
+    <a href="index.php?action=deconnecter">Se deconnecter</a>
+<?php else: ?>
+    <!-- Affiche le formulaire  -->
+    <form action="" method="post">
+        <div class="form-group m-3">
+            <input class="form-group mb-2" name="nom" placeholder="Entrer votre nom"> <br>
+            <button class="btn btn-primary  m-3">Se connecter</button>
+    </form>
+<?php endif; ?>
+</div>
+</div>
+</section>
+
+
+
+
+
+</body>
+
+</html>
