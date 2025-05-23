378bffc0574d8836e8fe7fe88e16fa77b04f41a8
Denismaka <makadenis370@gmail.com>
2025-05-23 17:29:08 +0100

    Mise en place du header pour mettre du boostrapp

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <title>Apprentissage des cookies avec php</title>
+</head>
+
+<body>
+</body>
+
+</html>
+
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
