10e2611e3454d57cb8c737042f8a1e2857163846
Denismaka <makadenis370@gmail.com>
2025-05-23 17:26:50 +0100

    correction de la syntaxe pour plus de lisibilibé du code

--- index.php
+++ index.php
@@ -1,57 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include('./header.php')
+?>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-    <title>Apprentissage des cookies avec php</title>
-</head>
+<!-- ############## -->
+<!-- cookies -->
+<!-- ############## -->
+<section class="cookie" id="cookie">
+    <div class="container">
+        <div class="row">
+            <?php
+            $nom = null;
+            if (!empty($_GET['action']) && $_GET['action'] === 'deconnecter') {
+                unset($_COOKIE['utilisateur']);
+                setcookie('utilisateur', '', time() - 10);
+            }
+            if (!empty($_COOKIE['utilisateur'])) {
+                $nom = $_COOKIE['utilisateur'];
+            }
+            if (!empty($_POST['nom'])) {
+                setcookie('utilisateur', $_POST['nom']);
+                $nom = $_POST['nom'];
+            }
 
-<body>
-    <!-- ############## -->
-    <!-- cookies -->
-    <!-- ############## -->
-    <section class="cookie" id="cookie">
-        <div class="container">
-            <div class="row">
-                <?php
-                $nom = null;
-                if (!empty($_GET['action']) && $_GET['action'] === 'deconnecter') {
-                    unset($_COOKIE['utilisateur']);
-                    setcookie('utilisateur', '', time() - 10);
-                }
-                if (!empty($_COOKIE['utilisateur'])) {
-                    $nom = $_COOKIE['utilisateur'];
-                }
-                if (!empty($_POST['nom'])) {
-                    setcookie('utilisateur', $_POST['nom']);
-                    $nom = $_POST['nom'];
-                }
+            ?>
 
-                ?>
-
-                <?php if ($nom): ?>
-                    <h1>Bonjour <?= htmlentities($nom) ?></h1>
-                    <a href="index.php?action=deconnecter">Se deconnecter</a>
-                <?php else: ?>
-                    <!-- Affiche le formulaire  -->
-                    <form action="" method="post">
-                        <div class="form-group m-3">
-                            <input class="form-group mb-2" name="nom" placeholder="Entrer votre nom"> <br>
-                            <button class="btn btn-primary  m-3">Se connecter</button>
-                    </form>
-                <?php endif; ?>
-            </div>
+            <?php if ($nom): ?>
+                <h1>Bonjour <?= htmlentities($nom) ?></h1>
+                <a href="index.php?action=deconnecter">Se deconnecter</a>
+            <?php else: ?>
+                <!-- Affiche le formulaire  -->
+                <form action="" method="post">
+                    <div class="form-group m-3">
+                        <input class="form-group mb-2" name="nom" placeholder="Entrer votre nom"> <br>
+                        <button class="btn btn-primary  m-3">Se connecter</button>
+                </form>
+            <?php endif; ?>
         </div>
-    </section>
-
-
-
-
-
-</body>
-
-</html>
-
-<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </div>
+</section>
