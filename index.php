6850d16c2678134d2775453cf090b12a4dbfe412
Denismaka <makadenis370@gmail.com>
2025-05-23 17:12:38 +0100

    Mise en place de la lecture du cookie avec  et setcookie pour en definir un

--- index.php
+++ index.php
@@ -8,11 +8,46 @@
     <title>Apprentissage des cookies avec php</title>
 </head>
 
-<body class="bg-dark">
-
+<body>
     <!-- ############## -->
     <!-- cookies -->
     <!-- ############## -->
+    <section class="cookie" id="cookie">
+        <div class="container">
+            <div class="row">
+                <?php
+                $nom = null;
+                if (!empty($_GET['action']) && $_GET['action'] === 'deconnecter') {
+                    unset($_COOKIE['utilisateur']);
+                    setcookie('utilisateur', '', time() - 10);
+                }
+                if (!empty($_COOKIE['utilisateur'])) {
+                    $nom = $_COOKIE['utilisateur'];
+                }
+                if (!empty($_POST['nom'])) {
+                    setcookie('utilisateur', $_POST['nom']);
+                    $nom = $_POST['nom'];
+                }
+
+                ?>
+
+                <?php if ($nom): ?>
+                    <h1>Bonjour <?= htmlentities($nom) ?></h1>
+                    <a href="index.php?action=deconnecter">Se deconnecter</a>
+                <?php else: ?>
+                    <!-- Affiche le formulaire  -->
+                    <form action="" method="post">
+                        <div class="form-group m-3">
+                            <input class="form-group mb-2" name="nom" placeholder="Entrer votre nom"> <br>
+                            <button class="btn btn-primary  m-3">Se connecter</button>
+                    </form>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+
+
+
 
 
 </body>
